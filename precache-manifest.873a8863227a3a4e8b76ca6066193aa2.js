db20029a5fcb44c520fd3d48cc724abc5c1d7230
Alc499 <Ali.vitchenko@gmail.com>
2019-07-11 16:50:46 +0300

    Updates

---
+++ precache-manifest.873a8863227a3a4e8b76ca6066193aa2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aaae70c40c42083fcba407a4c480eb9",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "af1f5528f72cac945620",
+    "url": "/react_people-table/static/css/main.479012fb.chunk.css"
+  },
+  {
+    "revision": "509cf8d9502ff8912294",
+    "url": "/react_people-table/static/js/2.cc6de668.chunk.js"
+  },
+  {
+    "revision": "af1f5528f72cac945620",
+    "url": "/react_people-table/static/js/main.e01ef82a.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_people-table/precache-manifest.3d8c6b1e7697442ca25d7e26bf334fd1.js"
+  "/react_people-table/precache-manifest.873a8863227a3a4e8b76ca6066193aa2.js"
 );
 
 self.addEventListener('message', (event) => {
